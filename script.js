ab98eb8d1db04c778fc49ca36b317618515cd89a
Fryderyk Boncler <f.relcnob@gmail.com>
2022-08-26 10:53:00 +0200

    fixed rounding, added clearing and showing previous outcomes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const calculateBtn = document.querySelector("#calculate");
+const clearBtn = document.querySelector("#clear");
 const resultList = document.querySelector("#results");
 
 calculateBtn.addEventListener("click", runCalculation);
+clearBtn.addEventListener("click", clearList);
 
 function runCalculation() {
   let firstNumber = document.querySelector("#firstnumber").value;
   let secondNumber = document.querySelector("#secondnumber").value;
   let operator = document.querySelector("#operator").value;
-  let doRounding = document.querySelector("#doround").checked;
   let decimals = document.querySelector("#decimals").value;
+  let doRounding = document.querySelector("#doround").checked;
   let outcome;
 
   // check for operator
@@ -25,13 +27,23 @@ function runCalculation() {
     outcome = Number(firstNumber) / Number(secondNumber);
     setValue(outcome);
   }
-  function setValue(e) {
-    if ((doRounding = "true")) {
-      document.querySelector("#firstnumber").value = e.toFixed(decimals);
-      document.querySelector("#secondnumber").value = "";
+  function setValue(outcome) {
+    if (doRounding == true) {
+      outcome = outcome.toFixed(decimals);
+      addToResults(outcome);
     } else {
-      document.querySelector("#firstnumber").value = e;
+      addToResults(outcome);
+    }
+    function addToResults(number) {
+      const listElement = document.createElement("li");
       document.querySelector("#secondnumber").value = "";
+      document.querySelector("#firstnumber").value = number;
+      listElement.textContent = number;
+      resultList.appendChild(listElement);
     }
   }
+}
+
+function clearList() {
+  resultList.innerHTML = "";
 }
